6806df499b494121f99cbbf4c01b6204308542e2
Marvis Dan <marvis.dan@gmail.com>
2018-07-29 17:48:34 +0200

    comment unused component

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Button from './Button';
-import UserBadge from './UserBadge';
+// import UserBadge from './UserBadge';
 
 
 const BookContainer = styled.div`
